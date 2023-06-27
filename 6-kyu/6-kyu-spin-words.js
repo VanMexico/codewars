9aa0b0427acaf7a9af4cd0e04f61be18e23cbe41
Ivan Pitertsev <van.mex@ya.ru>
2023-06-27 23:30:37 +0300

    6 kyu: Stop gninnipS My sdroW!
    https://www.codewars.com/kata/5264d2b162488dc400000001/solutions/javascript

---
+++ 6-kyu/6-kyu-spin-words.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001
+
+// my solution
+function spinWords(string) {
+  return string
+    .split(' ')
+    .map((word) =>
+      word.length >= 5 ? word.split('').reverse().join('') : word
+    )
+    .join(' ')
+}
